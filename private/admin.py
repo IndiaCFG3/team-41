74b2dbd1b39e316d497cff4119cac61f5f377533
SwathyD <swathy.dhanesh00@gmail.com>
2020-08-16 20:52:11 +0530

    removed unnecessary part

--- private/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from private.models import membership_status,ngo_staff
-# Register your models here.
-admin.site.register(membership_status)
-admin.site.register(ngo_staff)
--- public_/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from public_.models import User,Volunteer,Scheme
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Volunteer)
-admin.site.register(Scheme)

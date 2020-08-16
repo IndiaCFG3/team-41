ec3d1ba4ce9f6930c02477fcfc98dd7593d372de
uchauhan2022 <utkarshchauhan2022@gmail.com>
2020-08-16 19:35:35 +0530

    fix

--- untitled folder/sqldata.sql
+++ untitled folder/sqldata.sql
@@ -24,20 +24,20 @@ create table users (
 create table volunteers(
   id integer primary key autoincrement,
   name varchar(100) not null,
-  rcp_no integer not null,
-  date_ varchar(100) not null,
-  father varchar(100) not null,
-  mother varchar(100) not null,
-  dob varchar(100) not null,
-  age int not null,
-  gender varchar(100) not null,
-  phone varchar(100) not null,
+  rcp_no integer,
+  date_ varchar(100) ,
+  father varchar(100),
+  mother varchar(100) ,
+  dob varchar(100) ,
+  age int ,
+  gender varchar(100) ,
+  phone varchar(100) ,
   email varchar(100),
-  education varchar(100) not null,
-  address varchar(100) not null,
-  fam int not null,
-  occupation varchar not null,
-  monthly int not null
+  education varchar(100) ,
+  address varchar(100),
+  fam int ,
+  occupation varchar ,
+  monthly int
 );
 create table membership(
   name varchar(100) not null,
@@ -58,6 +58,7 @@ create table membership(
   userrole int not null
 );
 create table schemes(
+    id integer primary key autoincrement,
     name varchar(100) not null,
     description varchar(100) not null,
     lowerbound int not null,

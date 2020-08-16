ad1100f3896ecb53cd24611fda6dd53c77aa064c
uchauhan2022 <utkarshchauhan2022@gmail.com>
2020-08-16 12:39:42 +0530

    static files added

--- private/models.py
+++ private/models.py
@@ -3,10 +3,27 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Membership(AbstractUser):
+class membership_status(AbstractUser):
     Name = models.TextField(max_length = 100)
     Receipt_Number = models.AutoField(primary_key=True)
     Date = models.DateField(auto_now_add=True)
+    Father_Name = models.TextField(max_length = 100)
+    Mother_Nme = models.TextField(max_length = 100)
+    Dob = models.DateField()
+    Age = models.IntegerField()
+    Education = models.TextField(max_length =100)
+    Address = models.TextField(max_length = 100)
+    City = models.TextField(max_length = 100)
+    State = models.TextField(max_length = 100)
+    Family_members_count = models.IntegerField()
+    Occupation = models.TextField(max_length=100)
+    Montly = models.IntegerField()
+    Volunteer_id = models.IntegerField(null=True)
+
+class ngo_staff(AbstractUser):
+    Name = models.TextField(max_length = 100)
+    Enrollment_ID = models.AutoField(primary_key=True)
+    DateOfEnrollment = models.DateField(auto_now_add=True)
     Father_Name = models.TextField(max_length = 100)
     Mother_Nme = models.TextField(max_length = 100)
     Dob = models.DateField()
